a4767ddad762e65c713a71a486972cc27d7ca3d4
jeffersonfparil <jeffersonparil@gmail.com>
2021-01-05 15:17:40 +1100

    splitted LOL-WGS-0* into 2 so that we have enough RAM for BayesHammer error correction

--- illumina_01_QC_error_correct.sh
+++ illumina_01_QC_error_correct.sh
@@ -5,8 +5,8 @@
 
 ### Inputs:
 ### (1) Illumina reads in compressed fastq format (*.fastq.gz)
-### Notes: - all reads from 2020.11 were renamed from LOL-WGS2-{1..4}.fastq.gz into simply LOL-WGS-{1..4}.fastq.gz
-###        - LOL-WGS-*.fastq.gz from 2020.9 sequencing was renamed to LOL-WGS-0-*.fastq.gz
+### Notes: - all reads from 2020.11 were renamed from LOL-WGS2-{2..5}.fastq.gz into simply LOL-WGS-{2..5}.fastq.gz
+###        - LOL-WGS-*.fastq.gz from 2020.9 sequencing was split into 2 and renamed to LOL-WGS-{0..1}-*.fastq.gz
 
 ### Outputs:
 ### (1) Quality check html and zip files (*_fastqc.html and *_fastqc.zip)
@@ -28,7 +28,7 @@ mv *.zip QC/
 
 ### Bayesian error correction (may need to link python3 via: sudo ln -s /usr/bin/python3 /usr/bin/python)
 time \
-for i in 0 1 2 3 4
+for i in 0 1 2 3 4 5
 do
 # i=1
 mkdir BayesHammer_output_WGS-${i}/
