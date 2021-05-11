adb9b7faffaa2d5a8defd321cd29f1cbba9af3d0
jeffersonfparil <jeffersonparil@gmail.com>
2021-05-11 18:27:49 +1000

    struggling to install hypo for Lori_mw assembly polishing with both illumina and minion reads

--- assembly_01_error_correction.sh
+++ assembly_01_error_correction.sh
@@ -14,19 +14,54 @@ ASSEMBLY_FASTA=$1
 ## install
 DIR=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/
 cd $DIR
-# (1) install pilon
-wget https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar
-# (2) install minimap2
+# (1) install dependencies
+sudo apt-get update; sudo apt-get install build-essential software-properties-common -y;
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y; sudo apt update; 
+sudo apt install gcc-snapshot -y; sudo apt update
+sudo apt install gcc-8 g++-8 -y; 
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo apt install cmake
+# (2) install HTSLIB
+sudo -i
+cd /usr/bin
+wget https://github.com/samtools/htslib/releases/download/1.10/htslib-1.10.tar.bz2
+tar -vxjf htslib-1.10.tar.bz2
+cd htslib-1.10
+make
+logout
+export PATH="$PATH:/usr/bin/htslib-1.10"
+source ~/.profile
+# (3) install kmc for suk
+cd $DIR
+git clone https://github.com/refresh-bio/KMC
+cd KMC/
+make
+export PATH="$PATH:$(pwd)"
+source ~/.profile
+# (4) install hypo
+cd $DIR
+git clone --recursive https://github.com/kensung-lab/hypo hypo
+cd hypo
+chmod +x install_deps.sh
+./install_deps.sh
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -Doptimise_for_native=ON ..
+make -j 8
+# (5) install minimap2
 git clone https://github.com/lh3/minimap2
 cd minimap2 && make
 cd -
-# (3) install bwa
+# (6) install bwa
 sudo apt install bwa
+
+
+
 
 
 ### Genome assembly
 # ASSEMBLY=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/ASSEMBLY/Lori_mw/Lori_mw.raw.fa
-ASSEMBLY=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/ASSEMBLY/Lori_im/Lori_im.fasta
+ASSEMBLY=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/ASSEMBLY/Lori_mw/Lori_mw.fasta
 ### Navigate to working directory
 ASSEMBLY_DIR=$(dirname $ASSEMBLY)
 cd ${ASSEMBLY_DIR}
@@ -36,7 +71,9 @@ ASSEMBLY_NAME=$(echo ${ASSEMBLY_DIR} | rev | cut -d'/' -f1 | rev)
 bwa index -p ${ASSEMBLY_NAME} -a bwtsw ${ASSEMBLY}
 
 ## Align reads
-# (1) minion reads
+# (1) illumina reads
+
+# (2) minion reads
 MINION_READS=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/FASTQ/MINION/minion-filtered-trimmed.fastq.gz
 MINIMAP=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/minimap2/minimap2
 time \
@@ -51,47 +88,6 @@ samtools view -q ${MAPQ} -b ${ASSEMBLY_NAME}-MINION_READS.sam | \
 time \
 samtools index ${ASSEMBLY_NAME}-MINION_READS.bam
 
-### Pilon error correction
-PILON=/data/Lolium_rigidum_ASSEMBLY/assembly_annotation_pipeline_tests_20210104/pilon-1.24.jar
-MEM=250G
-THREADS=20
-
-### using minion alignments
-time \
-java -Xmx${MEM} -jar ${PILON} \
-    --genome ${ASSEMBLY} \
-    --unpaired ${ASSEMBLY_NAME}-MINION_READS.bam \
-    --output ${ASSEMBLY_NAME}-pilon \
-    --outdir ${ASSEMBLY_DIR} \
-    --tracks \
-    --diploid \
-    --verbose \
-    --threads ${THREADS}
 
 
-
-
-# (2) illumina reads
-time \
-bwa mem ${FNAME_REF} ${FNAME_READ1} ${FNAME_READ2} | \
-    samtools view -q ${MAPQ} -b | \
-    samtools sort > ${FNAME_OUT}.bam
-
-### using illumina alignments
-time \
-java -Xmx${MEM} -jar ${PILON} \
-    --genome ${ASSEMBLY} \
-    --frags ${ASSEMBLY_NAME}-ILLUMINA_READS.bam \
-    --output ${ASSEMBLY_NAME}-pilon \
-    --outdir ${ASSEMBLY_DIR} \
-    --tracks \
-    --diploid \
-    --verbose \
-    --threads 32
-
-
-
-
-
-### Quiver/Arrow
-git clone https://github.com/PacificBiosciences/GenomicConsensus.git
+### HYPO error correction
