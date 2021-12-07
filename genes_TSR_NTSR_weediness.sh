0a3d578f9393c2e6c147b61c19ebfc33afe2c312
jeff <jeffersonparil@gmail.com>
2021-12-07 18:57:17 +1100

    compiling TSR NTSR and weediness-related proteins

---
+++ genes_TSR_NTSR_weediness.sh
@@ -0,0 +1,60 @@
+##################
+### GLYPHOSATE ###
+##################
+### TARGET: 5-enolpyruvylshikimate-3-phosphate synthase
+###         Synonyms: 
+###             - EPSPS (in rice)
+###             - 3-phosphoshikimate 1-carboxyvinyltransferase (in rice and Arabidopsis)
+###             - epsp-s (in rice)
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+epsp&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" epsp'
+### Downloaded all 923 matches, and rename as:
+echo 'Glyphosate-target_EPSPS_UniProt_Mesangiospermae.fasta'
+
+#################
+### CLETHODIM ###
+#################
+### TARGET: Acetyle Co-A carboxylase
+###         Synonyms: 
+###             - ACCase
+###             - 
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+accase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" accase'
+### Downloaded all 10,446 matches, and rename as:
+echo 'Clethodim-target_ACCase_UniProt_Mesangiospermae.fasta'
+
+################
+### PARAQUAT ###
+################
+### TARGET: Photosytem I
+###         Notes: 
+###             - Paraquat steals electrons from photosynthesis, and diverts them to generate highly damaging reactive oxygen species.
+###             - Paraquat generates superoxide from water via the photosynthetic electron transport chain.
+###             - Chloroplastic superoxide dismutase (SOD) reduces superoxide into peroxide and oxygen.
+###             - Peroxide reacts with other superoxides to generate hydroxyl radicals.
+###             - These hydrogen radicals are highly reactive ad damages membrane lipids, DNA, proteins and even carbohydrates.
+###             -
+###             -
+###             -
+
+
+
+################
+### ATRAZINE ###
+################
+### TARGET: 
+###         Synonyms: 
+###             - 
+###             - 
+###             - 
+
+
+#################
+### INTERCEPT ### Imazamox, and
+################# Imazapyr
+### TARGET: Acetolactate synthase (ALS) or acetohydroxy acid synthase (AHAS)
+###         Synonyms: 
+###             - 
+###             - 
+###             - 
