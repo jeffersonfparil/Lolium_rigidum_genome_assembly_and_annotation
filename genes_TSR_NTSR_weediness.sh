b38951e9dbb62e8bf26b49e9d3d562e90d09886a
jeff <jeffersonparil@gmail.com>
2021-12-10 12:56:08 +1100

    compiled TSR and NTSR protein sequences

--- genes_TSR_NTSR_weediness.sh
+++ genes_TSR_NTSR_weediness.sh
@@ -1,3 +1,16 @@
+#################
+### CLETHODIM ###
+#################
+### TARGET: Acetyle Co-A carboxylase
+###         Synonyms: 
+###             - ACCase
+###             - 
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+accase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" accase'
+### Downloaded all 10,446 matches, and rename as:
+echo 'Clethodim-target_ACCase_UniProt_Mesangiospermae.fasta'
+
 ##################
 ### GLYPHOSATE ###
 ##################
@@ -12,21 +25,98 @@ echo 'taxonomy:"Mesangiospermae [1437183]" epsp'
 echo 'Glyphosate-target_EPSPS_UniProt_Mesangiospermae.fasta'
 
 #################
-### CLETHODIM ###
-#################
-### TARGET: Acetyle Co-A carboxylase
+### INTERCEPT ### Imazamox, and
+################# Imazapyr
+### TARGET: Acetolactate synthase a.k.a. acetohydroxy acid synthase
 ###         Synonyms: 
-###             - ACCase
+###             - ALS
+###             - AHAS
 ###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+als+ahas&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" als ahas'
+### Downloaded all 983 matches, and rename as:
+echo 'Intercept-target_ALS_UniProt_Mesangiospermae.fasta'
+
+###############
+### LUXIMAX ###
+############### Cinmethylin
+### TARGET: acyl-acp thioesterase
+###         Synonyms: 
+###             - ALT
 ###             - 
-### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+accase&sort=score):
-echo 'taxonomy:"Mesangiospermae [1437183]" accase'
-### Downloaded all 10,446 matches, and rename as:
-echo 'Clethodim-target_ACCase_UniProt_Mesangiospermae.fasta'
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+acyl-ACP+thioesterase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" acyl-acp thioesterase'
+### Downloaded all 1,978 matches, and rename as:
+echo 'Luximax-target_ALT_UniProt_Mesangiospermae.fasta'
+
+#################
+### OVERWATCH ###
+################# Bixlozone
+### TARGET: deoxyxylulose 5-phosphate synthase
+###         Synonyms: 
+###             - DXS
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+deoxyxylulose+5-phosphate+synthase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" deoxyxylulose 5-phosphate synthase'
+### Downloaded all 295 matches, and rename as:
+echo 'Overwatch-target_DXS_UniProt_Mesangiospermae.fasta'
+
+##############
+### SAKURA ###
+############## Pyroxasulfone
+### TARGET: very-long chain fatty acid elongase
+###         Synonyms: 
+###             - Very-long-chain enoyl-CoA reductase
+###             - Elongation of fatty acids protein
+###             - 3-ketoacyl-CoA synthase
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+very-long+chain+fatty+acid+elongase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" very-long chain fatty acid elongase'
+### Downloaded all 295 matches, and rename as:
+echo 'Sakura-target_VLCFAE_UniProt_Mesangiospermae.fasta'
+
+#################
+### TRIALLATE ###
+#################
+### TARGET: fatty acid elongase
+###         Synonyms: 
+###             - 3-ketoacyl-CoA synthase (same proteins as Sakura's: pyroxasulfone)
+###             - Elongation of fatty acids protein (same proteins as Sakura's: pyroxasulfone)
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+fatty+acid+elongase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" fatty acid elongase'
+### Downloaded all 311 matches, and rename as:
+echo 'Triallate-target_FAE_UniProt_Mesangiospermae.fasta'
+
+#################
+### BOXERGOLD ### S-metolachlor, and
+################# Prosulfocarb
+### TARGETS: very-long chain fatty acid elongase
+###         Synonyms: 
+###             - Very-long-chain enoyl-CoA reductase
+###             - 3-ketoacyl-CoA synthase (same proteins as Sakura's: pyroxasulfone)
+###             - Elongation of fatty acids protein (same proteins as Sakura's: pyroxasulfone)
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+very-long+chain+fatty+acid+elongase&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" very-long chain fatty acid elongase'
+### Downloaded all 295 matches, and rename as:
+echo 'BoxerGold-target_VLCFAE_UniProt_Mesangiospermae.fasta'
+
+################
+### ATRAZINE ###
+################
+### TARGET: Photosystem II protein D1
+###         Synonyms: 
+###             - psbA
+###             - 
+### UNIPROT QUERY (https://www.uniprot.org/uniprot/?query=taxonomy%3A%22Mesangiospermae+%5B1437183%5D%22+photosystem+2+protein+d1+%22psbA%22&sort=score):
+echo 'taxonomy:"Mesangiospermae [1437183]" photosystem 2 protein d1 psba'
+### Downloaded all 54,044 matches, and rename as:
+echo 'Atrazine-target_psbA_UniProt_Mesangiospermae.fasta'
 
 ################
 ### PARAQUAT ###
-################
+################ methylviologen
 ### TARGET: Photosytem I
 ###         Notes: 
 ###             - Paraquat steals electrons from photosynthesis, and diverts them to generate highly damaging reactive oxygen species.
@@ -34,27 +124,17 @@ echo 'Clethodim-target_ACCase_UniProt_Mesangiospermae.fasta'
 ###             - Chloroplastic superoxide dismutase (SOD) reduces superoxide into peroxide and oxygen.
 ###             - Peroxide reacts with other superoxides to generate hydroxyl radicals.
 ###             - These hydrogen radicals are highly reactive ad damages membrane lipids, DNA, proteins and even carbohydrates.
-###             -
-###             -
-###             -
+###             - References: Funderburk and Lawrence, 1964; and Li et al, 2013
+###             - Since their is no specific target enzyme it directly inhibits then we will serach for NTSR,
+###             - specifically detoxification enzymes:
+###                 1.) superoxide dismutase (SOD) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" superoxide dismutase' | 3,915 matches
+###                 2.) ascorbate peroxidase (APX) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" ascorbate peroxidase' | 2,823 matches
+###                 3.) dehydroascorbate reductase (GST) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" dehydroascorbate reductase' | 224 matches
+###                 4.) monodehydroascorbate reductase (MDAR) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" monodehydroascorbate reductase' | 820 matches
+###                 5.) glutathione peroxidase | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" glutathione peroxidase' | 2,174 matches
+###                 6.) L-type amino acid transporter (LAT) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" polyamine transporter rmv1' | 1,658 matches
+###                 7.) AtPDR11 (ATP-BINDING CASSETTE G39, ATPDR11, PDR11, PLEIOTROPIC DRUG RESISTANCE 11) | UNIPROT QUERY: 'taxonomy:"Mesangiospermae [1437183]" pdr abc' | 7,460 matches
+###                 8.) 
+echo 'Paraquat-target_NTSR_UniProt_Mesangiospermae.fasta'
 
 
-
-################
-### ATRAZINE ###
-################
-### TARGET: 
-###         Synonyms: 
-###             - 
-###             - 
-###             - 
-
-
-#################
-### INTERCEPT ### Imazamox, and
-################# Imazapyr
-### TARGET: Acetolactate synthase (ALS) or acetohydroxy acid synthase (AHAS)
-###         Synonyms: 
-###             - 
-###             - 
-###             - 
